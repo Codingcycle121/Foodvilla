630d70224b9cad43cae8764fa4a231e4ccb5c027
Codingcycle121 <paragbansal120@gmail.com>
2024-07-31 19:18:14 +0530

    new changes 7:18 31/07

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -39329,7 +39329,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62656" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52678" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
